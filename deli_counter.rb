9345cd434e04269a52620bf78f1b20bc7733195b
Forrest Benton <elliottbenton@gmail.com>
2018-07-23 23:34:43 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,10 +1,10 @@
 katz_deli = []
 
 def line(katz_deli)
-  if katz_deli.length == 0 
-    return "The line is currently empty."
+  if katz_deli.size == 0 
+    puts "The line is currently empty."
   else 
-    puts "stub"
+    
   end
 end
 
