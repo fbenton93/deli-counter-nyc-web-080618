a358336bc5dd1c182e87c30f43ee185660d3fd85
Forrest Benton <elliottbenton@gmail.com>
2018-07-23 23:41:58 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -7,7 +7,7 @@ def line(katz_deli)
     queue = ""
     i = 0 
     while i < katz_deli.length 
-      queue += (i + 1) + ". " + katz_deli[i] + ", "
+      queue += "#{i + 1}. #{katz_deli[i], "
       i += 1
     end
     puts "The line is currently: #{queue.slice(0,-2)}"
