cad9f8b364b46531cb0127ec7e23a820a316a826
Forrest Benton <elliottbenton@gmail.com>
2018-07-23 23:48:33 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -22,7 +22,7 @@ end
 
 def now_serving(katz_deli)
   if katz_deli.length == 0 
-    return "There is nobody waiting to be served!"
+    puts "There is nobody waiting to be served!"
   else 
     puts "Now serving #{katz_deli.shift}."
   end
